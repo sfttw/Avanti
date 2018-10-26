2b627a71d69b8186ebe273132ab5bd922ed274be
e7v <e7v@77night.club>
2018-10-26 15:17:33 -0700

    updated

--- modules/default.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf8 -*-
-'''example module'''
-import os
-import hashlib
-
-def html(params=None):
-	return '<b><h1>test</h1></b>'
--- modules/oldfarttv.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf8 -*-
-'''example module'''
-import os
-import hashlib
-
-def html(params=None):
-	return '<center><b><h1>OLDFAR TTV</h1></b></center>'
